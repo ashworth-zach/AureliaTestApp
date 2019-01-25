f5eed86dc7065f24755a92e18b5192f7d58bb94e
ashworth-zach <zashworth2@gmail.com>
2019-01-25 14:49:56 -0600

    progress

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
+import {inject} from 'aurelia-framework';
+import {DependencyTest} from './dependency-test';
+
+@inject(DependencyTest)
+
+
 export class App {
-  header:String;
-  content:String;
-  constructor(){
+  header: String;
+  content: String;
+  constructor(DependencyTest) {
     this.header = 'This is Header';
     this.content = 'This is content';
+    console.log(DependencyTest.test);
+
   }
   updateContent() {
     this.header = 'This is NEW header...'
@@ -12,5 +20,24 @@ export class App {
   resetContent() {
     this.header = 'This is Header';
     this.content = 'This is content';
+  }
+  created(owningView, myView) {
+    // Invoked once the component is created...
+    // this.header = "stuff"
+  }
+  bind(bindingContext, overrideContext) {
+    // Invoked once the databinding is activated...
+  }
+
+  attached(argument) {
+    // Invoked once the component is attached to the DOM...
+  }
+
+  detached(argument) {
+    // Invoked when component is detached from the dom
+  }
+
+  unbind(argument) {
+    // Invoked when component is unbound...
   }
 }
---
+++ src/dependency-test.ts
@@ -0,0 +1,6 @@
+export class DependencyTest {
+    test:String;
+    constructor() {
+       this.test = "Test is succesfull!!!";
+    }
+ }
